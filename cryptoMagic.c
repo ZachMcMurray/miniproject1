5f5dfab0b2b947cdb2eb2933b45f7c00590e5232
Zach McMurray <zmcmurray14@gmail.com>
2022-11-07 08:49:39 -0500

    added comments of how to run

--- cryptoMagic.c
+++ cryptoMagic.c
@@ -1,25 +1,8 @@
 #include <stdio.h>
 
 void encrypting(char argv[], int count){
-    char text;
-    int textNum;
-
-    if(count == 2){
-
-    FILE *textfile;
-
-        if((textfile = fopen(argv[1],'w+')) == NULL){
-        printf("file can not be opened");
-        return 0;
-        }
-
-        textfile = fopen(argv[1],'w+');
-
-        do{
-        text = fgetc(textfile);
-        }while(text != EOF);
-    }
-
+    // %x in prints gives hexadecimal
+   
 }
 
 void decrypting(char argv[]){
@@ -27,6 +10,17 @@ void decrypting(char argv[]){
 }
 
 int main(int argc, char *argv[]){
+
+    /*when running the code do it like .\"executable name" -E/-D "name of text file" 
+    exsample .\cryptoMagic -E myText.txt
+    argc is the number of strings in the enter line
+    .\cryptoMagic -E myText.txt 
+    here argc = 3
+    .\cryptoMagic myText.txt
+    here argc = 2
+    argv[] takes the strings entered in the run line
+    .\cryptoMagic -E myText.txt
+    here argv[0] = "cryptoMagic" argv[1] = "-E" argv[2] = "myText.txt"*/
 
     if(argc == 2){
     encrypting(argv, argc);
