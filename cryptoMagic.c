0eb155f7d1c8d961adf6eeb808c630c4ab24ccc3
Zach McMurray <zmcmurray14@gmail.com>
2022-11-18 22:40:32 -0500

    changed the switch statment

--- cryptoMagic.c
+++ cryptoMagic.c
@@ -163,110 +163,17 @@ void decrypting(FILE *writeFile,FILE *readFile){//function to decrypt
                 if(text1 == 'T' && text2 == 'T'){//checks if its a tab
                 fprintf(writeFile, "%c", 9);
                 }else{
-                    switch(text1){//get the first charater's face value
-                    case '0':
-                        text1FV = 0;
-                        break;
-                    case '1':
-                        text1FV = 1;
-                        break;
-                    case '2':
-                        text1FV = 2;
-                        break;
-                    case '3':
-                        text1FV = 3;
-                        break;
-                    case '4':
-                        text1FV = 4;
-                        break;
-                    case '5':
-                        text1FV = 5;
-                        break;
-                    case '6':
-                        text1FV = 6;
-                        break;
-                    case '7':
-                        text1FV = 7;
-                        break;
-                    case '8':
-                        text1FV = 8;
-                        break;
-                    case '9':
-                        text1FV = 9;
-                        break;
-                    case 'A':
-                        text1FV = 10;
-                        break;
-                    case 'B':
-                        text1FV = 11;
-                        break;
-                    case 'C':
-                        text1FV = 12;
-                        break;
-                    case 'D':
-                        text1FV = 13;
-                        break;
-                    case 'E':
-                        text1FV = 14;
-                        break;
-                    case 'F':
-                        text1FV = 15;
-                        break;
-                    default:
-                        printf("error");
+                    
+                    if (text1>=48&&text1<=57){
+                    text1FV =text1-48;
+                    }else if(text1>=65&&text1<=70){
+                    text1FV = text1 - 55;
                     }
 
-                    switch(text2){//get the second charater's face value
-                    case '0':
-                        text2FV = 0;
-                        break;
-                    case '1':
-                        text2FV = 1;
-                        break;
-                    case '2':
-                        text2FV = 2;
-                        break;
-                    case '3':
-                        text2FV = 3;
-                        break;
-                    case '4':
-                        text2FV = 4;
-                        break;
-                    case '5':
-                        text2FV = 5;
-                        break;
-                    case '6':
-                        text2FV = 6;
-                        break;
-                    case '7':
-                        text2FV = 7;
-                        break;
-                    case '8':
-                        text2FV = 8;
-                        break;
-                    case '9':
-                        text2FV = 9;
-                        break;
-                    case 'A':
-                        text2FV = 10;
-                        break;
-                    case 'B':
-                        text2FV = 11;
-                        break;
-                    case 'C':
-                        text2FV = 12;
-                        break;
-                    case 'D':
-                        text2FV = 13;
-                        break;
-                    case 'E':
-                        text2FV = 14;
-                        break;
-                    case 'F':
-                        text2FV = 15;
-                        break;
-                    default:
-                        printf("error");
+                    if (text2>=48&&text2<=57){
+                    text2FV =text2-48;
+                    }else if(text2>=65&&text2<=70){
+                    text2FV = text2 - 55;
                     }
 
                 text = (((text1FV*16)+text2FV)+16);//apply decryption key
